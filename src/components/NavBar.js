bfe53af77d5a0f91ce5da1c739cdd93ca872c44a
Jith <jith4j@gmail.com>
2021-06-27 12:59:06 +0530

    deploy

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+export default function NavBar() {
+  return (
+    <div className="navbar">
+      <ul>
+        <Link className="link" to="/">
+          Take me Home
+        </Link>
+      </ul>
+    </div>
+  );
+}
