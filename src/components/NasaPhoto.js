05169407dbf349ad217c8db5f28144eb13cb4231
Jith <jith4j@gmail.com>
2021-06-27 11:53:00 +0530

    getting started

---
+++ src/components/NasaPhoto.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+
+const apiKey = process.env.REACT_APP_NASA_KEY;
+
+export default function NasaPhoto() {
+  const [photoData, setPhotoData] = useState(null);
+
+  useEffect(() => {
+    fetchPhoto();
+
+    async function fetchPhoto() {
+      const res = await fetch(
+        `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`
+      );
+      const data = await res.json();
+      setPhotoData(data);
+      console.log(data);
+    }
+  }, []);
+
+  if (!photoData) return <div />;
+
+  return (
+    <div>
+      {photoData.media_type === "image" ? (
+        <img src={photoData.url} alt={photoData.title} />
+      ) : (
+        <iframe
+          title="space-video"
+          src={photoData.url}
+          frameBorder="0"
+          gesture="media"
+          allow="encrypted-media"
+          allowFullScreen
+          className="photo"
+        />
+      )}
+      <h1>{photoData.title}</h1>
+      <p>{photoData.date}</p>
+      <p>{photoData.explanation}</p>
+    </div>
+  );
+}
